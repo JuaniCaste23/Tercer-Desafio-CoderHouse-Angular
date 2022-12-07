c75db9a81ac99cb56f17ef3d6a5fd4f1ffbc409a
JuaniCaste23 <juanicaste23@gmail.com>
2022-12-06 22:02:16 -0300

    agregando angularMaterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormValidatorsComponent } from './components/form-validators/form-validators.component';
+import { FormComponent } from './components/form/form.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormValidatorsComponent,
+    FormComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/form-validators/form-validators.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {   } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-validators',
+  templateUrl: './form-validators.component.html',
+  styleUrls: ['./form-validators.component.css']
+})
+export class FormValidatorsComponent {
+
+}
